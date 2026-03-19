5b5afab0303e5bf4c6fa18fb3178e2b1f55c18e5
IsabelleLynch <isabellemarielynch@gmail.com>
2026-03-19 13:42:28 +0000

    test

--- Students.php
+++ Students.php
@@ -5,30 +5,16 @@
     <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="styles.css">
-    
-
   </head>
   <body>
-  
-    
-  
     <?php require_once 'Include/SideBar.php'; ?>
-    
-
     <main>
-        
-
-        
         <?php include_once("FormHandling.php"); ?>
         <div>
             <button id = "ShowForm">Add Student</button>
         </div>
-
         <?php include_once 'Include/GenerateTable.php'; ?>
-        
     </main>
   </body>
-  
   <script src="project.js"></script>
-  
 </html>
