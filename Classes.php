68d72dd4b0b77d8f1d29e1a5891a09eb70c15199
Isabelle Lynch <isabellemarielynch@gmail.com>
2026-03-21 17:53:12 +0000

    Messed up commit had to change

--- Classes.php
+++ Classes.php
@@ -15,19 +15,3 @@
   </body>
 </html>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Classes</title>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css">
-    <?php require_once('project.php'); ?>
-  </head>
-  <body>
-    <?php include 'SideBar.php'; ?>
-
-    Classes
-
-  </body>
-</html>
--- Dashboard.php
+++ Dashboard.php
@@ -15,19 +15,4 @@
   </body>
 </html>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Dashboard</title>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css">
-    <?php require_once('project.php'); ?>
-  </head>
-  <body>
-    <?php include 'SideBar.php'; ?>
 
-    Dashboard
-
-  </body>
-</html>
